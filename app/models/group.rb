b85fa1b5ada7f1aa9a5b1682843f1d4cbe5a8386
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-05-24 16:57:57 +0900

    modify group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :messages
 
-  validates :name, presence: true, unique: true
+  validates :name, presence: true
 
   def show_last_message
     if (last_message = messages.last).present?
