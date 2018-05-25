adf61e9c03f22be5c0aec5d6deb2a17276b58f71
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-05-25 18:57:31 +0900

    modify files
    非同期通信が作用しないため、作用するようファイルを修正した。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,28 @@
 $(function() {
   function buildHTML(message) {
+    if (message.image) {
+      var image = `<img src="${message.image}">`;
+    } else {
+      var image = '';
+    }
     var html = `
       <div class="message">
         <div class="message__user-name">
-          <%= ${message.user_name} %>
+          ${message.user_name}
         </div>
         <div class="message__posted-time">
-          <%= ${message.created_at} %>
+          ${message.created_at}
         </div>
         <div class="message__content">
-          <%= ${message.content} if ${message.content} %>
-          <%= image_tag ${message.image} if ${message.image} %>
+          <div class="content__text">
+            ${message.content}
+          </div>
+          <div class="content__image">
+            ${image}
+          </div>
         </div>
-      </div>`
+      </div>
+    `
     return html;
   }
 
@@ -24,14 +34,19 @@ $(function() {
       url: url,
       type: 'POST',
       data: formData,
-      dataType: 'json'
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data) {
       var html = buildHTML(data);
       $('.chat-main__body').append(html);
-      $('.message-content').val('');
-      $('.picture').val('');
-      $('.chat-main__body').animate({ scrollTop: $('.chat-main__body').height() }, 'swing');
+      $('#new_message')[0].reset();
+      $('.send-btn').prop('disabled', false);
+      $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'swing');
+    })
+    .fail(function() {
+      alert('メッセージが送信されませんでした');
     })
   })
 })
