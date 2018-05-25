0d8cea42a12d874d18b229fafb909b027408506d
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-05-25 16:55:49 +0900

    define function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,21 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `
+      <div class="message">
+        <div class="message__user-name">
+          <%= ${message.user_name} %>
+        </div>
+        <div class="message__posted-time">
+          <%= ${message.created_at} %>
+        </div>
+        <div class="message__content">
+          <%= ${message.content} if ${message.content} %>
+          <%= image_tag ${message.image} if ${message.image} %>
+        </div>
+      </div>`
+    return html;
+  }
+  
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
