8760eb18c362dd7c603b886d27ad8dadfbe63691
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-05-26 16:22:57 +0900

    delete searched_user
    選択したユーザを削除する機能を実装する。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,6 +20,17 @@ $(document).on('turbolinks:load', function() {
     search_list.append(html);
   }
 
+  function appendGroupUser(user_id, user_name) {
+    var html = `
+      <div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
+        <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+        <p class='chat-group-user__name'>${user_name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>
+    `
+    $('#chat-group-users').append(html);
+  }
+
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
     $.ajax({
@@ -41,5 +52,16 @@ $(document).on('turbolinks:load', function() {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  $('#user-search-result').on('click', '.user-search-add', function() {
+    $('#user-search-field').val('');
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    appendGroupUser(user_id, user_name);
+    $(this).parent().remove();
+  });
+  $('#chat-group-users').on('click', '.js-remove-btn', function() {
+    $(this).parent().remove();
   });
 });
